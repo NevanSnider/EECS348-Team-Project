4938613ab3d822b7c291e9bab6874b892db65070
Gaven Behrends <gzbehrends@gmail.com>
2024-12-12 13:21:14 -0600

    normalize test format

--- tests/cases.cpp
+++ tests/cases.cpp
@@ -5,75 +5,55 @@
 #include "cases.hpp"
 
 void TC01 () {
-    const std::string expr = "(3+(4*2))-5";
-    const std::string expected = "((3 + (4 * 2)) - 5)\n";
+    const string expr = "(3+(4*2))-5";
+    const string expected_tree = "((3 + (4 * 2)) - 5)";
 
+    // Test lexer and parser together
     Lexer* lexer = new Lexer({expr});
-    std::vector<Token> tokens = lexer->tokenization();
-    std::shared_ptr<ExpressionTree> tree = parse_expression(tokens);
-
-    std::ostringstream oss;
-    tree->printExpression(oss);
-    std::string repr = oss.str();
-
-    if (expected != repr) {
-        std::ostringstream error;
-        error << "Expected: " << expected << std::endl;
-        error << "Actual:   " << repr << std::endl;
-        delete lexer;
-        throw std::logic_error(error.str());
-    }
-
-    std::cout << "Actual: " << repr;
+    vector<Token> tokens = lexer->tokenization();
+    shared_ptr<ExpressionTree> tree = parse_expression(tokens);
     delete lexer;
+    string repr = tree_repr(tree);
+    if (expected_tree != repr) {
+        ostringstream error;
+        error << "Expected: " << expected_tree << endl;
+        error << "Actual:   " << repr << endl;
+        throw logic_error(error.str());
+    }
 }
 
 void TC02 () {
-    const std::string expr = "-3+6(4*-2)";
-    const std::string expected = "((-3) + (6 * (4 * (-2))))\n";
+    const string expr = "(-5)**2+(-5)**2";
+    const string expected_tree = "(((-5) ** 2) + ((-5) ** 2))";
 
+    // Test lexer and parser together
     Lexer* lexer = new Lexer({expr});
-    std::vector<Token> tokens = lexer->tokenization();
-    std::shared_ptr<ExpressionTree> tree = parse_expression(tokens);
-
-    std::ostringstream oss;
-    tree->printExpression(oss);
-    std::string repr = oss.str();
-
-    if (expected != repr) {
-        std::ostringstream error;
-        error << "Expected: " << expected << std::endl;
-        error << "Actual:   " << repr << std::endl;
-        delete lexer;
-        throw std::logic_error(error.str());
-    }
-
-    std::cout << "Actual: " << repr;
+    vector<Token> tokens = lexer->tokenization();
+    shared_ptr<ExpressionTree> tree = parse_expression(tokens);
     delete lexer;
+    string repr = tree_repr(tree);
+    if (expected_tree != repr) {
+        ostringstream error;
+        error << "Expected: " << expected_tree << endl;
+        error << "Actual:   " << repr << endl;
+        throw logic_error(error.str());
+    }
 }
 
 void TC03 () {
     const std::string expr = "-5*(3+(-2))^2";
-    const std::string expected = "((-5) * ((3 + (-2)) ** 2))\n";
+    const std::string expected_tree = "((-5) * ((3 + (-2)) ** 2))";
 
+    // Test lexer and parser together
     Lexer* lexer = new Lexer({expr});
-    std::vector<Token> tokens = lexer->tokenization();
-    std::shared_ptr<ExpressionTree> tree = parse_expression(tokens);
-
-    std::ostringstream oss;
-    tree->printExpression(oss);
-    std::string repr = oss.str();
-
-    if (expected != repr) {
-        std::ostringstream error;
-        error << "Expected: " << expected << std::endl;
-        error << "Actual:   " << repr << std::endl;
-        delete lexer;
-        throw std::logic_error(error.str());
-    }
-
-    std::cout << "Actual: " << repr;
+    vector<Token> tokens = lexer->tokenization();
+    shared_ptr<ExpressionTree> tree = parse_expression(tokens);
     delete lexer;
+    string repr = tree_repr(tree);
+    if (expected_tree != repr) {
+        ostringstream error;
+        error << "Expected: " << expected_tree << endl;
+        error << "Actual:   " << repr << endl;
+        throw logic_error(error.str());
+    }
 }
-
-
